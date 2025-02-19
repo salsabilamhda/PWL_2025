bc14aeeb18661d1717de1cb99bf5fba82fd7f6ef
salsabilamhda <salsabilamahda217@gmail.com>
2025-02-19 09:19:02 +0700

    Optional Parameters

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,11 @@ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
 Route::get('/articles/{id}', function ($id) {
     return "Halaman Artikel dengan ID " . $id;
 });
+
+Route::get('/user/{name?}', function ($name=null) { 
+    return 'Salsabila Mahda'.$name; 
+}); 
+
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name; 
+    });
