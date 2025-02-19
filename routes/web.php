4a251d6ba5b349964b7fd113c66655c542335c79
salsabilamhda <salsabilamahda217@gmail.com>
2025-02-19 08:47:46 +0700

    Basic Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/hello', function () { 
+return 'Hello World'; 
+}); 
+
+Route::get('/world', function () { 
+    return 'World'; 
+    }); 
+
+Route::get('/', function () { 
+    return 'Selamat Datang'; 
+    }); 
+
+
+Route::get('/about', function () { 
+    return '2341720257 - Salsabila Mahda Runisha'; 
+    }); 
