d5d395f74bc5d4626ba9a833a47ac8124605f537
salsabilamhda <salsabilamahda217@gmail.com>
2025-02-19 09:04:47 +0700

    Route Parameters

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,16 @@ Route::get('/', function () {
 Route::get('/about', function () { 
     return '2341720257 - Salsabila Mahda Runisha'; 
     }); 
+
+Route::get('/user/{name}', function ($name) { 
+return 'Nama saya '.$name; 
+});
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) { 
+return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID " . $id;
+});
